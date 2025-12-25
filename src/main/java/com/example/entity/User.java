93c33cb19bd038fedcbc32308be316b4567fbd38
onkarlondhe1 <onkarlondhe@outlook.com>
2025-12-25 10:02:42 +0530

    update user class

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class User {
 
     @Id
--- src/main/java/com/example/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/example/serviceimpl/UserServiceImpl.java
@@ -4,7 +4,9 @@ import com.example.entity.User;
 import com.example.repository.UserRepository;
 import com.example.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
